3c01b3cdf3de13ea98ce5cd41b9de1321b0ff218
karishmaupreti <karishmaupreti100@gmail.com>
2024-01-30 12:30:02 +0530

    Ecommerce

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,10 @@ const errorMiddleware=require("./middleware/error")
 const bodyParser=require("body-parser")
 const fileUpload=require("express-fileupload")
 const dotenv=require('dotenv')
-const path=require('path')
+//const path=require('path')
 
 //config
-if(process.env.NODE_ENV!=="PRODUCTION"){
-require("dotenv").config({path:"backend/config/config.env"})
-}
+dotenv.config({path:"backend/config/config.env"})
 
 app.use(express.json())
 app.use(cookieParser());
@@ -30,11 +28,11 @@ app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1",payment);
 
-app.use(express.static(path.join(__dirname,"../frontend/build")));
+// app.use(express.static(path.join(__dirname,"../frontend/build")));
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+// })
 
 //middleware for errors
 app.use(errorMiddleware);
